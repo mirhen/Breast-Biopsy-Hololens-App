88180ea0faebfd8e50a2528f4a366966e9bca057
Miriam <mjammer18@gmail.com>
2019-12-06 17:56:36 -0800

    first commit

---
+++ Biopsy Needle Project/Assets/NeedleScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NeedleScript : MonoBehaviour
+{
+
+    public float height;
+
+    Vector3 parentPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        parentPos = this.gameObject.transform.parent.position;
+       
+        Debug.Log(this.gameObject.transform.position);
+        Debug.Log(this.gameObject.transform.parent.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+
+        Vector3 newPos = new Vector3(parentPos.x, parentPos.y + height, parentPos.z);
+        this.gameObject.transform.position = newPos;
+    }
+}
